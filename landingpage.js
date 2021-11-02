eb7a58fdd2c97e902c9bbbda07bec07aa292e91a
bejibay <58639255+bejibay@users.noreply.github.com>
2021-11-02 14:26:15 +0100

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,6 +1,6 @@
 
 function displayIcon(){
-var x = document.getElementById("topnav");
+var x = document.getElementById("mytopnav");
 if(x.style.dispay==="block"){x.style.dispay==="none";}
 else{x.style.dispay==="block";}
 
